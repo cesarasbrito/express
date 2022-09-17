21d04b4c2723ef2c9fbc7449483eca81b5a89e76
Cesar Brito <cesarasbrito@gmail.com>
2022-09-17 12:35:16 -0300

    return file

--- myApp.js
+++ myApp.js
@@ -3,11 +3,13 @@ let app = express();
 
 console.log("Hello World");
 
-absolutePath = __dirname + "views/index.html"
+absolutePath = __dirname + "/views/index.html"
 
 app.get("/", function(req, res){
     //res.send('Hello Express');
     res.sendFile(absolutePath);
+
+    //res.sendFile(absolutePath);
 });
 
 
