c3a69f368340437f17fd8e16cf8ad6b395ad3ad6
Cesar Brito <cesarasbrito@gmail.com>
2022-09-17 13:07:55 -0300

    add json path response

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 
+app.get("json",function(req, res){
+   res.json({"message": "Hello json"});
+});
+
 app.get("/", function(req, res){
     //res.send('Hello Express');
     absolutePath = __dirname + "/views/index.html"
