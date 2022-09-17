37f5ee925064ece56392abc25144aace2cbffcab
Cesar Brito <cesarasbrito@gmail.com>
2022-09-17 19:26:07 -0300

    test string json

--- myApp.js
+++ myApp.js
@@ -1,11 +1,22 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+
 
 app.use("/public", express.static(__dirname + "/public"));
-
+value = process.env.MESSAGE_STYLE;
+console.log(value)
+console.log(process.env.MESSAGE_STYLE)
 
 app.get("/json",function(req, res){
-   res.json({"message": "Hello json"});
+
+    
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({"message": "HELLO JSON"});     
+    }
+    else{
+        res.json({"message": "Hello json"});
+    }
 });
 
 app.get("/", function(req, res){
