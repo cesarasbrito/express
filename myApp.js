a2480ed767bfc2a09569067591f1334b62a48616
Cesar Brito <cesarasbrito@gmail.com>
2022-09-17 13:09:04 -0300

    add json path response

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 
-app.get("json",function(req, res){
+app.get("/json",function(req, res){
    res.json({"message": "Hello json"});
 });
 
